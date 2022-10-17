6735718a820e872aa79cc9a05c07cd29cddc077c
Dr. Christoph "Schorsch" Jung <c-jung@t-systems.com>
2022-10-17 10:39:23 +0200

    https://jira.catena-x.net/browse/KA-124 first hacked version of the WebVowl ontology view

--- ux/skill_gym/src/App.tsx
+++ ux/skill_gym/src/App.tsx
@@ -1,11 +1,13 @@
-import { AssetView, OntologyView } from "@catenax-ng/skill-modules";
+import { AssetView, OntologyView, OntologyViewWebVowl } from "@catenax-ng/skill-modules";
 
 export default function App(){
-  const jsonUrl = 'https://raw.githubusercontent.com/catenax-ng/product-knowledge/main/infrastructure/consumer/resources/cx-ontology.json'
   return(
     <>
       <AssetView />
-      <OntologyView dataUrl={jsonUrl} />
+      {/*
+      <OntologyView dataUrl='https://raw.githubusercontent.com/catenax-ng/product-knowledge/main/infrastructure/consumer/resources/cx-ontology.json' />
+      */}
+      <OntologyViewWebVowl dataUrl='https://raw.githubusercontent.com/catenax-ng/product-knowledge/feature/KA-125-ontology-hub/ontology/cx.json'/>
     </>
   )
 }
--- ux/skill_modules/src/components/index.ts
+++ ux/skill_modules/src/components/index.ts
@@ -1,3 +1,4 @@
 export { AssetView } from "./AssetView";
 export { OntologyView } from "./OntologyView";
+export { OntologyViewWebVowl } from "./OntologyViewWebVowl";
 export { QueryEditor } from "./QueryEditor";
