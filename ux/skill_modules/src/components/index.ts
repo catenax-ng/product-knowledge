bebad594a33f781f149259f3ab6487ccc5e69e81
Janine Semmler <contact@janemcbrain.com>
2022-11-24 19:23:55 +0100

    remove webvowl component and add iframe instead

--- ux/skill_modules/src/components/index.ts
+++ ux/skill_modules/src/components/index.ts
@@ -2,5 +2,4 @@ export { AssetView } from './AssetView';
 export { CustomSearch } from './CustomSearch';
 export { DataList } from './DataList';
 export { OntologyView } from './OntologyView';
-export { OntologyViewWebVowl } from './OntologyViewWebVowl';
 export { QueryEditor } from './QueryEditor';
