8d6d0b6fdd2d53773008cd959c9165c5a942af88
Dr. Christoph "Schorsch" Jung <c-jung@t-systems.com>
2022-11-25 16:26:53 +0100

    https://jira.catena-x.net/browse/KA-143 configure two possible virtualization approaches.cd ..

---
+++ dataspace/agents/provisioning/src/main/java/it/unibz/inf/ontop/generation/normalization/impl/DruidNormalizer.java
@@ -0,0 +1,31 @@
+package it.unibz.inf.ontop.generation.normalization.impl;
+
+import com.google.inject.Inject;
+import it.unibz.inf.ontop.generation.normalization.DialectExtraNormalizer;
+import it.unibz.inf.ontop.iq.IQTree;
+import it.unibz.inf.ontop.utils.VariableGenerator;
+
+public class DruidNormalizer implements DialectExtraNormalizer {
+
+    private final TypingNullsInUnionDialectExtraNormalizer typingNullInUnionNormalizer;
+    private final TypingNullsInConstructionNodeDialectExtraNormalizer typingNullInConstructionNormalizer;
+    private final SubQueryFromComplexJoinExtraNormalizer complexJoinNormalizer;
+
+    @Inject
+    protected DruidNormalizer(TypingNullsInUnionDialectExtraNormalizer typingNullInUnionNormalizer,
+                                    TypingNullsInConstructionNodeDialectExtraNormalizer typingNullInConstructionNormalizer,
+                                    SubQueryFromComplexJoinExtraNormalizer complexJoinNormalizer) {
+        this.typingNullInUnionNormalizer = typingNullInUnionNormalizer;
+        this.typingNullInConstructionNormalizer = typingNullInConstructionNormalizer;
+        this.complexJoinNormalizer = complexJoinNormalizer;
+    }
+
+    @Override
+    public IQTree transform(IQTree tree, VariableGenerator variableGenerator) {
+        return complexJoinNormalizer.transform(
+                typingNullInConstructionNormalizer.transform(
+                        typingNullInUnionNormalizer.transform(tree, variableGenerator),
+                        variableGenerator),
+                variableGenerator);
+    }
+}
