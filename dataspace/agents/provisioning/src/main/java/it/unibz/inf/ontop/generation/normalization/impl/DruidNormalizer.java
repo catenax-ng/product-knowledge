4fc714b45be0acce206be4ddb130af705485bcc6
Dr. Christoph "Schorsch" Jung <c-jung@t-systems.com>
2022-11-28 17:06:19 +0100

    https://jira.catena-x.net/browse/KA-141 prevent >32k fields. do not use ontop 5 code.

--- dataspace/agents/provisioning/src/main/java/it/unibz/inf/ontop/generation/normalization/impl/DruidNormalizer.java
+++ dataspace/agents/provisioning/src/main/java/it/unibz/inf/ontop/generation/normalization/impl/DruidNormalizer.java
@@ -1,3 +1,9 @@
+//
+// Application to provide REST APIs as SPARQL services
+// See copyright notice in the top folder
+// See authors file in the top folder
+// See license file in the top folder
+//
 package it.unibz.inf.ontop.generation.normalization.impl;
 
 import com.google.inject.Inject;
@@ -5,27 +11,26 @@ import it.unibz.inf.ontop.generation.normalization.DialectExtraNormalizer;
 import it.unibz.inf.ontop.iq.IQTree;
 import it.unibz.inf.ontop.utils.VariableGenerator;
 
+/**
+ * Ontop Normalizer which makes the druid optimizer happy.
+ * TODO single joins per value binding
+ */
 public class DruidNormalizer implements DialectExtraNormalizer {
 
-    private final TypingNullsInUnionDialectExtraNormalizer typingNullInUnionNormalizer;
-    private final TypingNullsInConstructionNodeDialectExtraNormalizer typingNullInConstructionNormalizer;
+    private final TypingNullsDialectExtraNormalizer typingNullNormalizer;
     private final SubQueryFromComplexJoinExtraNormalizer complexJoinNormalizer;
 
     @Inject
-    protected DruidNormalizer(TypingNullsInUnionDialectExtraNormalizer typingNullInUnionNormalizer,
-                                    TypingNullsInConstructionNodeDialectExtraNormalizer typingNullInConstructionNormalizer,
-                                    SubQueryFromComplexJoinExtraNormalizer complexJoinNormalizer) {
-        this.typingNullInUnionNormalizer = typingNullInUnionNormalizer;
-        this.typingNullInConstructionNormalizer = typingNullInConstructionNormalizer;
+    public DruidNormalizer(TypingNullsDialectExtraNormalizer typingNullNormalizer,
+                              SubQueryFromComplexJoinExtraNormalizer complexJoinNormalizer) {
+        this.typingNullNormalizer = typingNullNormalizer;
         this.complexJoinNormalizer = complexJoinNormalizer;
     }
 
     @Override
     public IQTree transform(IQTree tree, VariableGenerator variableGenerator) {
         return complexJoinNormalizer.transform(
-                typingNullInConstructionNormalizer.transform(
-                        typingNullInUnionNormalizer.transform(tree, variableGenerator),
-                        variableGenerator),
+                typingNullNormalizer.transform(tree, variableGenerator),
                 variableGenerator);
     }
 }
