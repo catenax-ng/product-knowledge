e72a901560e0de8894a8bb0a6e7af362fb794665
Dr. Christoph Schorsch Jung <christoph.jung@jasna.de>
2022-11-29 11:24:54 +0000

    https://jira.catena-x.net/browse/KA-141 Stuff runs locally. Now go for deployment.

--- infrastructure/resources/dremio/3_classifiedloadcollective.sql
+++ infrastructure/resources/dremio/3_classifiedloadcollective.sql
@@ -1,13 +1,14 @@
-DROP TABLE $scratch.CX_RUL_Testdata_v1_LoadCollective;
-CREATE TABLE $scratch.CX_RUL_Testdata_v1_LoadCollective AS (
+DROP TABLE $scratch.CX_RUL_LoadCollective;
+CREATE TABLE $scratch.CX_RUL_LoadCollective AS (
     SELECT
         SUBSTR(JSON.catenaXId,10) as catenaXId,
         SUBSTR(JSON.json.targetComponentID,10) as targetComponentId,
         JSON.json['metadata'].projectDescription as metadata_projectDescription,
         JSON.json['metadata'].componentDescription as metadata_componentDescription,
-        TO_DATE(LEFT(JSON.json['metadata'].status['date'],10),'YYYY-MM-DD') as metadata_status_date,
+        TO_TIMESTAMP(LEFT(JSON.json['metadata'].status['date'],10),'YYYY-MM-DD') as metadata_status_date,
         CAST(JSON.json['metadata'].status.operatingTime AS DOUBLE) as metadata_status_operatingTime,
         CAST(JSON.json['metadata'].status.mileage AS DOUBLE) as metadata_status_mileage,
+        JSON.json.header.countingValue as header_countingValue,
         JSON.json.header.countingUnit as header_countingUnit,
         JSON.json.header.countingMethod as header_countingMethod,
         CONVERT_TO(JSON.json.header.channels,'json') as header_channels,
@@ -21,5 +22,19 @@ CREATE TABLE $scratch.CX_RUL_Testdata_v1_LoadCollective AS (
     ) JSON
     WHERE JSON.json IS NOT NULL
 );
-DROP VIEW "HI_TEST_OEM".CX_RUL_Testdata_v1_LoadCollective;
-CREATE VIEW "HI_TEST_OEM".CX_RUL_Testdata_v1_LoadCollective AS SELECT * FROM $scratch.CX_RUL_Testdata_v1_LoadCollective;
+DROP VIEW "HI_TEST_OEM".CX_RUL_LoadCollective;
+CREATE VIEW "HI_TEST_OEM".CX_RUL_LoadCollective AS 
+SELECT catenaXId,
+       targetComponentId,
+       metadata_projectDescription,
+       metadata_componentDescription,
+       TO_CHAR(metadata_status_date,'YYYY-MM-DD"T"HH:MI:SS.FFF"Z"') as metadata_status_date,
+       CAST(metadata_status_operatingTime AS INTEGER) as metadata_status_operatingTime,
+       CAST(metadata_status_mileage AS INTEGER) as metadata_status_mileage,
+       header_countingValue,
+       header_countingUnit,
+       header_countingMethod,
+       header_channels,
+       body_counts,
+       body_classes
+  FROM $scratch.CX_RUL_LoadCollective;
