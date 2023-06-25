d60239cf6a1434b24a92826de445467ca2ae7a63
arnoweiss <arno.weiss@sap.com>
2023-06-25 12:46:31 +0200

    iterate submodels in materialForRecycling by distinct eMat

--- dataspace/aas-bridge/src/main/java/io/catenax/knowledge/dataspace/aasbridge/MappingExecutor.java
+++ dataspace/aas-bridge/src/main/java/io/catenax/knowledge/dataspace/aasbridge/MappingExecutor.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.common.base.Charsets;
 import com.google.common.io.CharStreams;
+import com.sun.xml.bind.v2.util.QNameMap;
 import io.adminshell.aas.v3.model.AssetAdministrationShellEnvironment;
 import org.eclipse.digitaltwin.aas4j.exceptions.TransformationException;
+import org.eclipse.digitaltwin.aas4j.mapping.MappingSpecificationParser;
 import org.eclipse.digitaltwin.aas4j.mapping.model.MappingSpecification;
 import org.eclipse.digitaltwin.aas4j.transform.GenericDocumentTransformer;
 
@@ -16,11 +18,15 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
+import java.util.AbstractMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
 
@@ -34,68 +40,89 @@ public class MappingExecutor {
     private final int timeoutSeconds;
     private final int fixedThreadPoolSize;
 
+    private Map<File, MappingSpecification> queryMappingAssignment;
+
 
     public MappingExecutor(URI sparqlEndpoint, String credentials, int timeoutSeconds, int fixedThreadPoolSize) {
+        this.queryMappingAssignment = getMappingsFromResources();
+
         this.transformer = new GenericDocumentTransformer();
         this.providerSparqlEndpoint = sparqlEndpoint;
         this.credentials = credentials;
         this.timeoutSeconds = timeoutSeconds;
         this.fixedThreadPoolSize = fixedThreadPoolSize;
+
+
+
     }
 
-    public AssetAdministrationShellEnvironment executeMapping(String query, MappingSpecification mappingSpecification) {
+    private Map<File, MappingSpecification> getMappingsFromResources() {
         try {
-            InputStream queryResult = executeQuery(query).get();
-            transformer.execute(queryResult, mappingSpecification);
+            return Files.walk(Path.of("src/main/resources/selectQueries/"))
+                    .filter(obj -> !obj.endsWith("selectQueries"))
+                    .map(obj -> {
+                        String mappingFileFolder = obj.getParent().getParent().toString() + "/mappingSpecifications/";
+                        String mappingFileName = obj.getFileName().toString().split("-")[0] + "-mapping.json";
+                        try {
+                            MappingSpecification spec =
+                                    new MappingSpecificationParser().loadMappingSpecification(mappingFileFolder+mappingFileName);
+                            return new AbstractMap.SimpleEntry<>(obj, spec);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .collect(Collectors.toMap(e -> e.getKey().toFile(), e -> e.getValue()));
 
-
-        } catch (URISyntaxException | InterruptedException | ExecutionException | IOException e) {
-            throw new RuntimeException(e);
-        } catch (TransformationException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    /**
+     * @param queryFile    file holding the respective sparql-query
+     * @param mappingSpecification mapping specification to apply a query result to a submodel template
+     * @return the resulting AAS Environment contains multiple AAS with a single submodel each.
+     */
+    public AssetAdministrationShellEnvironment executeMapping(File queryFile, MappingSpecification mappingSpecification) {
+        try {
+            InputStream queryResult = executeQuery(queryFile).get();
+            transformer.execute(queryResult, mappingSpecification);
 
-        // execute query
-        // parse, convert to xml
-        // parse mapping
-        // execute
+        } catch (URISyntaxException | InterruptedException | ExecutionException | IOException |
+                 TransformationException e) {
+            throw new RuntimeException(e);
+        }
 
         return null;
     }
 
     /**
-     * @param queryResourcePath the relative path to the sparql query starting at the resources folder
+     * @param queryFile the file containing the query, probably loaded from resources
      * @return xml structure of the query response
      * @throws URISyntaxException
      * @throws IOException
      */
-    public CompletableFuture<InputStream> executeQuery(String queryResourcePath) throws URISyntaxException, IOException {
-        InputStream stream = MappingExecutor.class.getResourceAsStream(queryResourcePath);
-        if (stream != null) {
-            try (InputStreamReader reader = new InputStreamReader(stream, Charsets.UTF_8)) {
-                String query = CharStreams.toString(reader);
-                HttpRequest.BodyPublisher bodyPublisher = HttpRequest.BodyPublishers.ofString(query);
-                HttpRequest.Builder requestBuilder = HttpRequest.newBuilder().uri(providerSparqlEndpoint).POST(bodyPublisher).header("Content-Type", "application/sparql-query").header("Accept", "application/xml").timeout(Duration.of(timeoutSeconds, SECONDS));
+    public CompletableFuture<InputStream> executeQuery(File queryFile) throws URISyntaxException, IOException {
+        String query = Files.readAllLines(queryFile.toPath()).get(0);
 
-                if (credentials != null && !credentials.isEmpty()) {
-                    requestBuilder = requestBuilder.header("Authorization", credentials);
-                }
+        HttpRequest.BodyPublisher bodyPublisher = HttpRequest.BodyPublishers.ofString(query);
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder().uri(providerSparqlEndpoint).POST(bodyPublisher).header("Content-Type", "application/sparql-query").header("Accept", "application/xml").timeout(Duration.of(timeoutSeconds, SECONDS));
 
-                HttpRequest request = requestBuilder.build();
-
-                return HttpClient.newBuilder().executor(Executors.newFixedThreadPool(fixedThreadPoolSize)).build().sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(res -> {
-                    if (res.statusCode() >= 200 && res.statusCode() < 300) {
-                        return res.body();
-                    } else {
-                        throw new RuntimeException("Sparql-Request failed with " + res.statusCode() + res.body());
-                    }
-                }).thenApply(body -> new ByteArrayInputStream(body.getBytes()));
-            }
-        } else {
-            throw new RuntimeException("query resource stream is empty!");
+        if (credentials != null && !credentials.isEmpty()) {
+            requestBuilder = requestBuilder.header("Authorization", credentials);
         }
 
+        HttpRequest request = requestBuilder.build();
+
+        return HttpClient.newBuilder().executor(Executors.newFixedThreadPool(fixedThreadPoolSize)).build().sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(res -> {
+            if (res.statusCode() >= 200 && res.statusCode() < 300) {
+                return res.body();
+            } else {
+                throw new RuntimeException("Sparql-Request failed with " + res.statusCode() + res.body());
+            }
+        }).thenApply(body -> new ByteArrayInputStream(body.getBytes()));
     }
+
 }
+
 
--- dataspace/aas-bridge/src/test/java/io/catenax/knowledge/dataspace/aasbridge/MappingExecutorTest.java
+++ dataspace/aas-bridge/src/test/java/io/catenax/knowledge/dataspace/aasbridge/MappingExecutorTest.java
@@ -1,17 +1,24 @@
 package io.catenax.knowledge.dataspace.aasbridge;
 
+import io.adminshell.aas.v3.model.AssetAdministrationShellEnvironment;
+import io.adminshell.aas.v3.model.Property;
+import io.adminshell.aas.v3.model.Submodel;
+import io.adminshell.aas.v3.model.impl.DefaultProperty;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import org.eclipse.digitaltwin.aas4j.exceptions.TransformationException;
+import org.eclipse.digitaltwin.aas4j.mapping.MappingSpecificationParser;
+import org.eclipse.digitaltwin.aas4j.mapping.model.MappingSpecification;
+import org.eclipse.digitaltwin.aas4j.transform.GenericDocumentTransformer;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
@@ -19,6 +26,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MappingExecutorTest {
@@ -27,11 +35,27 @@ class MappingExecutorTest {
             "?OemProviderAgent=" +
             URLEncoder.encode("http://oem-provider-agent:8082/sparql", StandardCharsets.ISO_8859_1);
 
-    void executeMapping() {
+    @Test
+    @Disabled
+    void executeMapping() throws IOException, TransformationException {
+        MappingSpecification mapping = new MappingSpecificationParser().loadMappingSpecification("src/main/resources/mappingSpecifications/materialForRecycling-mapping.json");
+        GenericDocumentTransformer transformer = new GenericDocumentTransformer();
+        InputStream instream = MappingExecutorTest.class.getResourceAsStream("/sparqlResponseXml/MaterialForRecycling-sparql-results.xml");
+        String s = new String(instream.readAllBytes());
+        AssetAdministrationShellEnvironment env = transformer.execute(new ByteArrayInputStream(s.getBytes()), mapping);
+        assertEquals(12, env.getSubmodels().size());
+        env.getAssetAdministrationShells().forEach(aas ->
+                assertTrue(aas.getAssetInformation().getGlobalAssetId().getKeys().get(0).getValue().startsWith("urn:material")));
+
+        env.getSubmodels().stream().map(sm -> getProperty(sm, "materialName")).anyMatch(p -> p.equals("bla"));
+        env.getSubmodels().stream().map(sm -> getProperty(sm, "materialClass")).anyMatch(p -> p.equals("CeramicMaterial"));
+
+        // env.getSubmodels().forEach(sm -> assertEquals("EngineeringMaterial", getProperty(sm, "materialClass")));
 
     }
 
 
+    @Disabled
     @ParameterizedTest
     @ValueSource(strings = {"MaterialForRecycling", "PartAsPlanned", "PartSiteInformation", "SingleLevelBomAsPlanned"})
     void executeQuery(String aspectName) throws IOException, URISyntaxException, ExecutionException, InterruptedException {
@@ -42,7 +66,9 @@ class MappingExecutorTest {
                 3,
                 5 );
 
-        InputStream inputStream = executor.executeQuery("/selectQueries/select" + aspectName + ".rq").get();
+        InputStream inputStream = executor.executeQuery(
+                new File(MappingExecutor.class.getClassLoader().getResource("/selectQueries/select" + aspectName + ".rq").getFile()))
+                .get();
         String result = new String(inputStream.readAllBytes());
         assertEquals(result, getMockResponseBody(aspectName));
     }
@@ -65,5 +91,14 @@ class MappingExecutorTest {
                 .getResourceAsStream("sparqlResponseXml/"+ aspectName +"-sparql-results.xml")
                 .readAllBytes());
         return mockResponseBody;
+    }
+
+    private String getProperty(Submodel submodel, String propertyIdShort) {
+        return submodel.getSubmodelElements().stream()
+                .filter(sme->sme.getClass().equals(DefaultProperty.class))
+                .map(sme->(Property)sme)
+                .filter(p->p.getIdShort().equals(propertyIdShort))
+                .findFirst().map(Property::getValue)
+                .orElseThrow(()-> new RuntimeException("propertyNotFound"));
     }
 }
